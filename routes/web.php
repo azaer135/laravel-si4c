13947f792cdd97dc327c684ec22e42742281cf4f
azaer135 <mdzakyabdurrahman_2428240148p@mhs.mdp.ac.id>
2026-04-27 13:12:52 +0700

    NEW AAAAAAAAA

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FakultasController; // Add this import
+use App\Http\Controllers\PeriodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,6 @@ Route::get('/', function () {
 });
 
 Route::resource('Fakultas', FakultasController::class);
+Route::resource('/Fakultas', FakultasController::class);
+
+Route::resource('/periode', PeriodeController::class);
