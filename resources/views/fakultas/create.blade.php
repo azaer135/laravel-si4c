27b5c778331e44f1241547d512fc31de51cc3202
azaer135 <mdzakyabdurrahman_2428240148p@mhs.mdp.ac.id>
2026-05-18 12:39:39 +0700

    bantuan zikri 1

--- resources/views/fakultas/create.blade.php
+++ resources/views/fakultas/create.blade.php
@@ -1,12 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('main')
+@section('title', 'Tambah Fakultas')
+@section('content')
+
+        <form action="{{ route('fakultas.store') }}" method="post">
+            @csrf
+            <div class= "form-group">
+                <label for="nama" class="form-label">Nama Fakultas</label>
+                <input type="text" class="form-control" name="nama" value="{{ old('nama') }}">
+                @error("nama")
+                    <div class='text-danger'>{{ $message }}</div>
+                @enderror
+            </div>
+            <div class= "form-group">
+                <label for="singkatan" class="form-label">Singkatan</label>
+                <input type="text" class="form-control" name="singkatan" value="{{ old('singkatan') }}">
+                @error("singkatan")
+                    <div class='text-danger'>{{ $message }}</div>
+                @enderror
+            </div>
+            <div class= "form-group">
+                <label for="dekan" class="form-label">Nama Dekan</label>
+                <input type="text" class="form-control" name="dekan"  value="{{ old('dekan') }}">
+                @error("dekan")
+                    <div class='text-danger'>{{ $message }}</div>
+                @enderror
+            </div>
+                
+            <button type="submit" class="btn btn-primary mt-3">Simpan</button>
+        </form>
+
+@endsection
